16d1f6dee8bb374c3d1e3bb3e26c7a2dec5597db
Vanessa Stewart <vstewart1213@gmail.com>
2021-04-28 10:13:05 -0700

    Add mongoose and update root query

--- server/app.js
+++ server/app.js
@@ -2,8 +2,20 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(port))
+  .catch((err) => console.log(err))
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
